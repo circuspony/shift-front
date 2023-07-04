dd116c6ac4f9407dcbe1a13cd606135260d51ca3
Aws <exibp@mail.ru>
2023-07-04 14:24:22 +0700

    profile

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,33 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home'
 import Profile from './pages/profile'
+import Header from './components/layout/header';
+import SignIn from './pages/signin';
+import SignUp from './pages/signup';
+import { UserContextProvider } from './components/context/userContext';
+import { useEffect, useState } from 'react';
+import Cookies from 'universal-cookie';
 
 function App() {
+  const [token, setToken] = useState(undefined)
+  useEffect(() => {
+    const cookies = new Cookies();
+    if (cookies.get('token')) {
+      setToken(cookies.get('token'))
+    }
+  }, [])
   return (
-    <>
-
-      <Routes>
-        <Route path="/profile" element={<Profile />} />
-        <Route path={"*"} element={<Home />} />
-      </Routes>
-    </>
+    <div className='w-full h-full flex flex-col'>
+      <UserContextProvider token={token}>
+        <Header />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path={"*"} element={<Home />} />
+        </Routes>
+      </UserContextProvider>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "button-green": "#D2E892",
+        "button-green-text": "#364C6F",
+        "custom-grey": "rgb(230,230,230)",
+        "input-border": "#D6E1E8"
+      },
+      backgroundImage: {
+        'gradient-bg': 'linear-gradient(90deg, rgba(28,52,91,0.05) 0%, rgba(23,73,151,0.0295) 100%);',
+      },
+    },
   },
   plugins: [],
 }
