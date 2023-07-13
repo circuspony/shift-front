8c42ccf2d29b3fb30910e0521534f1714ab549ee
Aws <exibp@mail.ru>
2023-07-13 14:58:37 +0700

    added dropdown arrow

--- src/components/inputs/select.tsx
+++ src/components/inputs/select.tsx
@@ -1,4 +1,6 @@
 import { useState, Dispatch, SetStateAction } from 'react';
+import Arrow from '../images/downarrow.svg';
+
 interface SelectOption {
   label: string;
   value: string;
@@ -18,9 +20,16 @@ const CustomSelect = ({
     <>
       <div className='w-full relative cursor-pointer border-solid border-2 rounded-2xl bg-transparent mt-4 outline-none'>
         <div className='w-full'>
-          <div onClick={() => setDropDownOpen(!dropDownOpen)} className='w-full p-4'>
+          <div
+            onClick={() => setDropDownOpen(!dropDownOpen)}
+            className='w-full p-4 grid grid-cols-2'
+          >
             {option.label}
+            <div className='flex items-center justify-end'>
+              <img src={Arrow} alt='Arrow' className='w-4 h-4 top-0 left-0' />
+            </div>
           </div>
+
           <div
             className={`dropdown-scroll absolute border-solid border-2 rounded-lg top-14 h-48 overflow-scroll w-full py-4 px-2 left-0 overflow-x-hidden bg-white z-50 ${
               dropDownOpen ? 'flex flex-col' : 'hidden'
