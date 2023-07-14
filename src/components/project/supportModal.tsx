0983bab4a6f0ec0e1adce6c0b020514a35a8fcd3
circuspony <endofrever@gmail.com>
2023-07-14 18:33:25 +0700

    fix: small support visual fix

--- src/components/project/supportModal.tsx
+++ src/components/project/supportModal.tsx
@@ -5,12 +5,13 @@ import Button from "../button"
 import Modal from "../modal/modal"
 import { useForm, FieldValues } from "react-hook-form";
 import PiggyIcon from "../svg/piggyIcon";
+import CoinIcon from "../svg/coinIcon";
 
 
 function SupportModal({ id, isModal, onClose, updateProject }: { id: string | undefined, isModal: boolean, onClose: Function, updateProject: Function }) {
     const { userInfo, updateUserProfile } = useAuth()
     const [success, setSuccess] = useState(false)
-    const { register, formState: { errors }, handleSubmit, setError } = useForm({});
+    const { register, formState: { errors }, handleSubmit, setError, getValues } = useForm({});
 
     const onSubmit = async (data: FieldValues) => {
         const supportStatus = await supportProject({
@@ -37,13 +38,20 @@ function SupportModal({ id, isModal, onClose, updateProject }: { id: string | un
                 title='Заголовок'
                 content={<div className='overflow-auto'>
                     {success &&
-                        <div className='p-4'>
-                            <div className='text-3xl mt-4 text-center'>Успешно поддержано! Спасибо!</div>
+                        <div className='flex flex-col p-4'>
+                            <div className='text-3xl font-bold mt-4 text-center'>Спасибо!</div>
+                            <span className='mx-auto flex items-center text-xl mt-4 text-center'>
+                                {"Ваши " + getValues("amount")}
+                                <div className="h-5 mr-2">
+                                    <CoinIcon />
+                                </div>
+                                {" точно не пропадут зря!"}
+                            </span>
                             <div className='w-56 mx-auto my-4'><PiggyIcon /></div>
                         </div>}
                     {!success &&
                         <div className='p-4'>
-                            <div className='text-3xl mt-4 text-center'>Поддержать проект</div>
+                            <div className='text-3xl mt-4 font-bold text-center'>Поддержать проект</div>
                             <form onSubmit={handleSubmit(onSubmit)} autoComplete='off' className='flex flex-col'>
                                 <div className='flex items-center mt-4'>
                                     <input
